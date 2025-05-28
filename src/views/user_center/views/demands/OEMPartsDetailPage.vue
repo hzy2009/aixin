2fb2157bfdf1a4f4b01f16137ad4616439af01f0
chenzihong <chenzihong@51qqt.com>
2025-05-28 15:11:53 +0800

    fix

--- src/views/user_center/views/demands/OEMPartsDetailPage.vue
+++ src/views/user_center/views/demands/OEMPartsDetailPage.vue
@@ -24,20 +24,24 @@ const isManagerAdmin = computed(() => {
 const router = useRouter();
 // // --- 表单配置 ---
 const formConfigs = [
-  {
-    label: '需求提出方', field: 'tenantName', fieldType: 'input', span: 24, disabled: true,
-  },
   { label: '计划完成日期', field: 'expireDate', fieldType: 'date', span: 24, disabled: isManagerAdmin.value },
   { label: '需求数量', field: 'reqPartsTotal', fieldType: 'number', span: 24, disabled: isManagerAdmin.value },
+  {label: '需求提出方', field: 'tenantName', fieldType: 'input', span: 24, disabled: true,},
   {
     label: '需求状态', field: 'statusCode', detailField: 'statusName', fieldType: 'select', dictKey: 'sourcing_status', span: 24, disabled: !isManagerAdmin.value,
     onChange: ({ value, form, options }) => {
       form.statusName = options.find(opt => opt.value === value)?.label || '';
     }
   },
-  { label: '已寻到数量', field: 'reqPartsFinish', fieldType: 'number', span: 24, disabled: true },
-  { label: '未寻到数量', field: 'reqPartsUnfinish', fieldType: 'number', span: 24, disabled: true },
-]
+  { label: '已寻到数量', field: 'reqPartsFinish', fieldType: 'number', span: 24, disabled: true, mode: 'view' },
+  { label: '未寻到数量', field: 'reqPartsUnfinish', fieldType: 'number', span: 24, disabled: true, mode: 'view'  },
+].filter(item => {
+    if (props.mode == 'create' && item.mode == 'view') {
+      return false;
+    }
+    return true;
+  }
+);
 
 const statusHistoryColumns = [
   { title: '序号', dataIndex: 'seq', key: 'seq', width: 60, align: 'center' },
--- src/views/user_center/views/demands/PublicRelationsDetailPage.vue
+++ src/views/user_center/views/demands/PublicRelationsDetailPage.vue
@@ -30,17 +30,23 @@ const formConfigs = [
       form.rdType = options.find(opt => opt.value === value)?.label || '';
     }
   },
-  { label: '创建日期', field: 'createDate', fieldType: 'date', span: 24, disabled: true },
+  { label: '创建日期', field: 'createDate', fieldType: 'date', span: 24, disabled: true, mode: 'view' },
   { label: '研发需求', field: 'sourceDesc', fieldType: 'input', span: 24 },
-  {
-    label: '期望匹配周期', field: 'period', fieldType: 'select', dictKey: 'rd_breakthrough_period', span: 24,
-  },
   {
     label: '需求状态', field: 'statusCode', detailField: 'statusName', fieldType: 'select', dictKey: 'rd_breakthrough_status', span: 24, disabled: !isManagerAdmin.value
   },
-  { label: '需求有效期', field: 'expireDate', fieldType: 'date', span: 24 },
+  {
+    label: '期望匹配周期', field: 'period', fieldType: 'select', dictKey: 'rd_breakthrough_period', span: 24,
+  },
+  // { label: '需求有效期', field: 'expireDate', fieldType: 'date', span: 24 },
   { label: '需求提出方', field: 'tenantName', fieldType: 'input', span: 24, disabled: true, },
-]
+].filter(item => {
+    if (props.mode == 'create' && item.mode == 'view') {
+      return false;
+    }
+    return true;
+  }
+);
 
 const statusHistoryColumns = [
   { title: '序号', dataIndex: 'seq', key: 'seq', width: 60, align: 'center' },
