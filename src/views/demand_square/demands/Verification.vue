5b3cefd5476aea45e049b164513bfcb27c894401
chenzihong <chenzihong@51qqt.com>
2025-07-04 15:56:56 +0800

    fix

--- src/views/demand_square/demands/Verification.vue
+++ src/views/demand_square/demands/Verification.vue
@@ -23,7 +23,7 @@ const getALLProductTypes = () => {
 }
 // --- Filter Configuration (remains in component as it's UI specific) ---
 const filterConfigForPage = reactive([
-    { id: 'projectType', label: '验证验证类型', maxVisibleWithoutMore: 7, dictKey: 'project_type' },
+    { id: 'projectType', label: '检测验证类型', maxVisibleWithoutMore: 7, dictKey: 'project_type' },
     { id: 'productType', label: '产品类别', maxVisibleWithoutMore: 13, dictKey: 'product_type', options: getALLProductTypes() },
 ]);
 
