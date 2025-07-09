fdd6a39806e690fa96092e44255132b55f450989
chenzihong <chenzihong@51qqt.com>
2025-07-09 11:28:38 +0800

    fix

--- src/views/user_center/views/join/Verification/list.vue
+++ src/views/user_center/views/join/Verification/list.vue
@@ -18,7 +18,7 @@ const router = useRouter();
 const getALLProductTypes = () => {
    const allProductTypes = selectOptions('product_type')  
    const allProductTypeMaterials = selectOptions('product_type_material')
-   return [...allProductTypes, ...allProductTypeMaterials]
+   return [{ value: '', label: '全部' }, ...allProductTypes, ...allProductTypeMaterials]
 }
 // --- Filter Configuration (remains in component as it's UI specific) ---
 const filterConfigForPage = reactive([
