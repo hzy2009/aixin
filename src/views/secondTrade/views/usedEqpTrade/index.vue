87c3a83eccc87f8ca70c1ba8fbd01d29e9bd3ac1
chenzihong <chenzihong@51qqt.com>
2025-07-27 23:00:53 +0800

    fix

--- src/views/secondTrade/views/usedEqpTrade/index.vue
+++ src/views/secondTrade/views/usedEqpTrade/index.vue
@@ -42,12 +42,12 @@ const handleClick = () => {
 const handleFinish = async (p) => {
     phoneAndEmailModal.value.handleClose()
     const defaultConfig = {
-    title: '一键敲门成功',
-    message: '一键敲门后，客服人员将在30分钟内与您联系',
-    contactInfo: { name: '陈靖玮', phone: '4000118892', email: 'info-service@icshare.com' },
-    buttonText: '返回首页',
-    showButton: false,
-    onAction: null, // Default onAction is handled in store to go home
+        title: '一键敲门成功',
+        message: '一键敲门后，客服人员将在30分钟内与您联系',
+        contactInfo: { name: '陈靖玮', phone: '4000118892', email: 'info-service@icshare.com' },
+        buttonText: '返回首页',
+        showButton: false,
+        onAction: null, // Default onAction is handled in store to go home
     };
     modalStore.showSuccessPrompt({ ...defaultConfig });
     // const res = await defHttp.post({
@@ -67,16 +67,6 @@ const handleFinish = async (p) => {
     //     };
     //     modalStore.showSuccessPrompt({ ...defaultConfig });
     // }
-    phoneAndEmailModal.value.handleClose()
-    const defaultConfig = {
-    title: '一键敲门成功',
-    message: '一键敲门后，客服人员将在30分钟内与您联系',
-    contactInfo: { name: '陈靖玮', phone: '4000118892', email: 'info-service@icshare.com' },
-    buttonText: '返回首页',
-    showButton: false,
-    onAction: null, // Default onAction is handled in store to go home
-    };
-    modalStore.showSuccessPrompt({ ...defaultConfig });
 }
 const tableOperations = reactive([
     {
