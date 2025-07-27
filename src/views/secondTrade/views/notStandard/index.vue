b61415aef8e489f60a8040e2bde71148da709581
chenzihong <chenzihong@51qqt.com>
2025-07-27 22:58:24 +0800

    fix: 假敲门

--- src/views/secondTrade/views/notStandard/index.vue
+++ src/views/secondTrade/views/notStandard/index.vue
@@ -40,7 +40,16 @@ const handleClick = () => {
 }
 
 const handleFinish = async (p) => {
-    message.info('功能正在实现，敬请期待')
+    phoneAndEmailModal.value.handleClose()
+    const defaultConfig = {
+    title: '一键敲门成功',
+    message: '一键敲门后，客服人员将在30分钟内与您联系',
+    contactInfo: { name: '陈靖玮', phone: '4000118892', email: 'info-service@icshare.com' },
+    buttonText: '返回首页',
+    showButton: false,
+    onAction: null, // Default onAction is handled in store to go home
+    };
+    modalStore.showSuccessPrompt({ ...defaultConfig });
     // const res = await defHttp.post({
     //     url: `/apm/apmDeviceUnstandard/newTodo`,
     //     params: p,
