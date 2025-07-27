aa344e24ff24d32589a2ba4dd00fdfb1755fe037
chenzihong <chenzihong@51qqt.com>
2025-07-27 22:57:08 +0800

    fix: 假数据

--- src/views/secondTrade/views/oemParts/index.vue
+++ src/views/secondTrade/views/oemParts/index.vue
@@ -40,23 +40,24 @@ const handleClick = () => {
 }
 
 const handleFinish = async (p) => {
-    const res = await defHttp.post({
-        url: `/apm/apmDeviceOrigin/newTodo`,
-        params: p,
-        data: ids.value
-    });
-    if (res.success) {
-        phoneAndEmailModal.value.handleClose()
-        const defaultConfig = {
-        title: '一键敲门成功',
-        message: '一键敲门后，客服人员将在30分钟内与您联系',
-        contactInfo: { name: '陈靖玮', phone: '4000118892', email: 'info-service@icshare.com' },
-        buttonText: '返回首页',
-        showButton: false,
-        onAction: null, // Default onAction is handled in store to go home
-        };
-        modalStore.showSuccessPrompt({ ...defaultConfig });
-    }
+    message.info('功能正在实现，敬请期待')
+    // const res = await defHttp.post({
+    //     url: `/apm/apmDeviceOrigin/newTodo`,
+    //     params: p,
+    //     data: ids.value
+    // });
+    // if (res.success) {
+    //     phoneAndEmailModal.value.handleClose()
+    //     const defaultConfig = {
+    //     title: '一键敲门成功',
+    //     message: '一键敲门后，客服人员将在30分钟内与您联系',
+    //     contactInfo: { name: '陈靖玮', phone: '4000118892', email: 'info-service@icshare.com' },
+    //     buttonText: '返回首页',
+    //     showButton: false,
+    //     onAction: null, // Default onAction is handled in store to go home
+    //     };
+    //     modalStore.showSuccessPrompt({ ...defaultConfig });
+    // }
 }
 const tableOperations = reactive([
     {
@@ -73,6 +74,218 @@ const pageData = ref({
     },
     tableColumns,
     tableOperations,
+        localTableData: [
+        {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+         {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+        {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+         {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+        {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+         {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+        {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+         {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+        {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+         {
+            partNumber: 'KC-312156',
+            productName: 'MS-400',
+            productCategory: '产品类别',
+            productModel: '产品型号',
+            specification: '5x10-9m',
+            compatibleModels: 'MS-400',
+            originalManufacturer: '*******有限公司',
+            productionDate: '2025-08-29',
+            productStatus: '正常状态',
+            quantity: '35080',
+            postedBy: 'B15675',
+            guaranteePeriod: '1年',
+            priceExcludingTax: '58000',
+            tax: '12%',
+            paymentTermsName: '付款条件名称',
+            tradeTypeName: '交易方式名称',
+            shippingTypeName: '物流方式名称',
+            usageDuration: '使用时长',
+            description: '描述'
+        },
+    ]
     // requiredRoles: ['apm-vip', 'apm-vip-inspection', 'apm-register'],
 })
 
